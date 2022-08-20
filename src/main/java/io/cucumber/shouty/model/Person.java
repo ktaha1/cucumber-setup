83c90ce05cc62139c4183cdbf85523fc129ea4bb
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-20 12:31:14 +0100

    Complete Person entity implementation

--- src/main/java/io/cucumber/shouty/model/Person.java
+++ src/main/java/io/cucumber/shouty/model/Person.java
@@ -1,18 +1,37 @@
 package io.cucumber.shouty.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+    private List<String> messageShouted;
+    private List<String> messagesHeard;
+    private int distance;
+    private final int range=20;
     public void moveTo(Integer distance) {
+        this.distance=distance;
 
     }
 
     public void shout(String message) {
-
+        if (this.messageShouted==null) this.messageShouted = new ArrayList<>();
+        this.messageShouted.add(message);
     }
 
-    public List<String> getMessagesHeard() {
-        return null;
+
+    public void hear(List<String> messageShouted) {
+        if (distance<=range){
+            if (this.messagesHeard==null) this.messagesHeard = new ArrayList<>();
+            this.messagesHeard.addAll(messageShouted);
+        }
     }
 }
