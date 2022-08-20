6e5b8afa4f05c5c3af773a805c0983c4dca99a2f
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-20 12:32:19 +0100

    Finish implementing step definitions

--- src/test/java/io/cucumber/shouty/StepDefinitions.java
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -6,27 +6,26 @@ import io.cucumber.java.en.When;
 import io.cucumber.shouty.model.Person;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.Arrays;
-
 public class StepDefinitions {
-    private Person sean;
-    private Person lucy;
-    private String messageFromSean;
+    private final Person sean;
+    private final Person lucy;
+
+    public StepDefinitions() {
+        this.sean = new Person();
+        this.lucy = new Person();
+    }
 
     @Given ("Lucy is located {int} metres from Sean")
     public void lucy_is_located_metres_from_sean(Integer distance) {
-        System.out.println("Distance : "+distance);
-        lucy = new Person();
-        sean = new Person();
         lucy.moveTo(distance);
     }
     @When("Sean shouts {string}")
     public void sean_shouts(String message) {
         sean.shout(message);
-        messageFromSean = message;
     }
     @Then("Lucy hears Sean's message")
     public void lucy_hears_sean_s_message() {
-        Assertions.assertEquals(Arrays.asList(messageFromSean), lucy.getMessagesHeard());
+        lucy.hear(sean.getMessageShouted());
+        Assertions.assertEquals(sean.getMessageShouted(), lucy.getMessagesHeard());
     }
 }
