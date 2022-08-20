7bf9ce9f839b6cbb1ceca78e039bbc2c40aa637f
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-20 11:58:15 +0100

    Implement step definitions methods

---
+++ src/main/java/io/cucumber/shouty/model/Person.java
@@ -0,0 +1,18 @@
+package io.cucumber.shouty.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Person {
+    public void moveTo(Integer distance) {
+
+    }
+
+    public void shout(String message) {
+
+    }
+
+    public List<String> getMessagesHeard() {
+        return null;
+    }
+}
--- src/test/java/io/cucumber/shouty/StepDefinitions.java
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -3,22 +3,30 @@ package io.cucumber.shouty;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.shouty.model.Person;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
 
 public class StepDefinitions {
+    private Person sean;
+    private Person lucy;
+    private String messageFromSean;
+
     @Given ("Lucy is located {int} metres from Sean")
     public void lucy_is_located_metres_from_sean(Integer distance) {
         System.out.println("Distance : "+distance);
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        lucy = new Person();
+        sean = new Person();
+        lucy.moveTo(distance);
     }
     @When("Sean shouts {string}")
-    public void sean_shouts(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void sean_shouts(String message) {
+        sean.shout(message);
+        messageFromSean = message;
     }
     @Then("Lucy hears Sean's message")
     public void lucy_hears_sean_s_message() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assertions.assertEquals(Arrays.asList(messageFromSean), lucy.getMessagesHeard());
     }
 }
