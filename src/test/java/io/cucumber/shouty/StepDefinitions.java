fc508ccc87295622fa902e62ec90784d2dc3253b
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-20 11:41:31 +0100

    Add stepDefinitions class

---
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -0,0 +1,24 @@
+package io.cucumber.shouty;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given ("Lucy is located {int} metres from Sean")
+    public void lucy_is_located_metres_from_sean(Integer distance) {
+        System.out.println("Distance : "+distance);
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Sean shouts {string}")
+    public void sean_shouts(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Lucy hears Sean's message")
+    public void lucy_hears_sean_s_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
